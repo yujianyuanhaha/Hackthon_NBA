c823bbe68d2bb53ef924c542f1e1708950574b4d
yaolin <cherry.figh0918@gmail.com>
2019-07-14 01:05:30 -0400

    init

---
+++ myTest.m
@@ -0,0 +1,34 @@
+Play_by_Play = readtable('mini_Play_by_Play.csv');
+Game_Lineup = readtable('mini_Game_Lineup.csv');
+Event_Codes = readtable('Event_Codes.csv');
+
+num_games = height(unique(Game_Lineup(:,1)));
+num_teams = height(unique(Game_Lineup(:,4)));
+num_players = height(unique(Game_Lineup(:,3)));
+
+% ========== toy code: calculate point for each player =====
+% player-points table
+player = unique(Game_Lineup(:,3));
+score = array2table(zeros(num_players,1));
+% Player_Score = table(player,score);  % nor join()
+Player_Score = [player,score];
+
+
+
+tic;
+for i = 1:height(Play_by_Play)
+    if Play_by_Play{i,8} > 0
+        playerName = Play_by_Play{i,12};
+        
+        for j = 1:height(Player_Score)
+            if strcmp(Player_Score{j,1}, playerName)
+                Player_Score{j,2} =  Player_Score{j,2} + Play_by_Play{i,8};
+            end
+        end                
+    end
+    
+    if mod(i,1000) == 0
+        disp(i);
+        toc;
+    end
+end
