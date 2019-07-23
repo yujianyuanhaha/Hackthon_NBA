8aeba7ea0770aec6b3fe1c07dfcd75c0d37f5bef
yaolin <cherry.figh0918@gmail.com>
2019-07-22 22:47:32 -0400

    geting closer

--- getStart10.m
+++ getStart10.m
@@ -8,11 +8,17 @@ numPeriod = (height(Game_Lineup)-30)/10;
 
 Start5 = array2table(cell(numPeriod*2,8));
 for i = 1:numPeriod*2
-    temp = Game_Lineup( 30+(i-1)*5+[1:5] ,4);
-    Game_Lineup( 30+(i-1)*5+1, 4:8) =  array2table(table2array(temp)');
-    Start5(i,4:8) =  array2table(table2array(temp)');
-    Start5(i,1)= Game_Lineup(30+ (i-1)*5+1,1);
-    Start5(i,3)= Game_Lineup( 30+(i-1)*5+1,3);
+%     temp = Game_Lineup( 30+(i-1)*5+[1:5] ,4);
+%     Game_Lineup( 30+(i-1)*5+1, 4:8) =  array2table(table2array(temp)');
+%     Start5(i,4:8) =  array2table(table2array(temp)');
+%     Start5(i,1)= Game_Lineup(30+ (i-1)*5+1,1);
+%     Start5(i,3)= Game_Lineup( 30+(i-1)*5+1,3);
+
+    temp = Game_Lineup{ 30+(i-1)*5+[1:5] ,4};
+    Game_Lineup{ 30+(i-1)*5+1, 4:8} = temp';
+    Start5{i,4:8} =  temp';
+    Start5{i,1}= Game_Lineup{30+ (i-1)*5+1,1};
+    Start5{i,3}= Game_Lineup{ 30+(i-1)*5+1,3};
 end
 Start5(:,2)=[];
 temp =repmat([1:numPeriod],2,1);
